41f077e6ada2bfedb4cbca64a61f1065df65efb6
ssktgm <51650332+ssktgm@users.noreply.github.com>
2025-12-08 12:47:20 +0000

    bug fix

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // 相対パスに変更
+//  base: './', // 相対パスに変更
   plugins: [react()],
 })
